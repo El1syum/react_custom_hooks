7153c296ccac00429bd65c1cc614b79c46a9b21a
el1syum <kilmol606@gmail.com>
2023-05-08 19:42:33 +0300

    initial

---
+++ src/hooks/useInput.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+const UseInput = (initialValue) => {
+    const [value, setValue] = useState(initialValue)
+
+    const onChange = e => {
+        setValue(e.target.value)
+    }
+
+    return {value, onChange}
+};
+
+export default UseInput;
